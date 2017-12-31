8c00b528f2f4050f42a8bf08cb65147691fb9feb
yuanleilei628 <yuanleilei628@163.com>
2017-12-31 12:22:26 +0800

    同步与异步

---
+++ thread/src/com/base/sync003/MultiThread.java
@@ -0,0 +1,44 @@
+package com.base.sync003;
+
+/**
+ * 同步和异步
+ */
+public class MultiThread {
+
+	public synchronized void method1() {
+		try {
+			System.err.println(Thread.currentThread().getName());
+			Thread.sleep(4000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void method2() {
+		System.err.println(Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) {
+
+		MultiThread mt = new MultiThread();
+
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				mt.method1();
+			}
+
+		}, "t1");
+
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				mt.method2();
+			}
+		}, "t2");
+
+		t1.start();
+		t2.start();
+	}
+
+}
